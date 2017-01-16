22a2749268ba131a17e98bea1a15e86e4569ea2b
Aline Vianna <alinealvesvianna@gmail.com>
2017-01-16 00:07:18 -0200

    criandos tasks de compressão de arquivos.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,7 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
+
+    // var css = grunt.file.read(cssfilepath[, options]);
+
     grunt.initConfig({
 
         responsive_images: {
@@ -6,18 +9,20 @@ module.exports = function (grunt) {
                 options: {
                     engine: 'im',
                     sizes: [{
+                        name: 'small',
                         width: 320,
-                        quality: 40
+                        suffix: '_x1',
+                        quality: 30
                     }, {
                         name: 'medium',
                         width: 640,
                         suffix: '_x2',
-                        quality: 40
+                        quality: 30
                     }, {
                         name: 'large',
                         width: 1024,
                         suffix: '_x3',
-                        quality: 40
+                        quality: 30
                     }]
                 },
 
@@ -26,34 +31,56 @@ module.exports = function (grunt) {
                     src: ['*.{gif,jpg,png}'],
                     cwd: 'src/img/',
                     dest: 'dist/img/'
-                }],
-
-                filesViews: [{
+                }, {
                     expand: true,
                     src: ['*.{gif,jpg,png}'],
-                    cwd: 'views/src/',
-                    dest: 'views/dist/'
+                    cwd: 'src/img/img-web/',
+                    dest: 'dist/img/img-web/'
+                }, {
+                    expand: true,
+                    src: ['*.{gif,jpg,png}'],
+                    cwd: 'views/src/images/',
+                    dest: 'views/dist/images/'
+                }]
+            }
+        },
+        imagemin: {
+            dynamic: {
+                options: {
+                    optimizationLevel: 3,
+                    svgoPlugins: [{
+                        removeViewBox: false
+                    }],
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'dist/img/',
+                    src: ['*.{png,jpg,gif}'],
+                    dest: 'dist/img/'
+                }, {
+                    expand: true,
+                    cwd: 'dist/img/img-web/',
+                    src: ['*.{png,jpg,gif}'],
+                    dest: 'dist/img/img-web/'
+                }, {
+                    expand: true,
+                    cwd: 'views/dist/images/',
+                    src: ['*.{png,jpg,gif}'],
+                    dest: 'views/dist/images/'
                 }]
             }
         },
 
         clean: {
-            img: {
-                src: ['dist/img/', 'views/dist/img']
+            dist: {
+                src: ['dist', 'views/dist']
             },
-            css: {
-                src: ['dist/css/', 'views/dist/css']
-            },
-            js: {
-                src: ['dist/js/', 'views/dist/js']
-            }
         },
-
 
         mkdir: {
             img: {
                 options: {
-                    create: ['dist/img/', 'views/dist/img']
+                    create: ['dist/img/', 'views/dist/images', 'dist/img/img-web']
                 }
             },
             css: {
@@ -68,29 +95,29 @@ module.exports = function (grunt) {
             }
         },
 
-        concat: {
-            files: {
-                src: 'src/js/*.js',
-                dest: 'dist/js/scripts.js'
-            },
-            filesView: {
-                src: 'views/src/js/*.js',
-                dest: 'views/dist/js/scriptsViews.js'
-            }
-        },
+        // concat: {
+        //     files: {
+        //         src: 'src/js/*.js',
+        //         dest: 'dist/js/scripts.js'
+        //     },
+        //     filesView: {
+        //         src: 'views/src/js/*.js',
+        //         dest: 'views/dist/js/scriptsViews.js'
+        //     }
+        // },
 
 
         minified: {
             files: {
                 src: [
-                'dist/js/*.js'
+                    'src/js/*.js'
                 ],
                 dest: 'dist/js/'
             },
 
             filesView: {
                 src: [
-                'views/dist/js/*.js'
+                    'views/src/js/*.js'
                 ],
                 dest: 'views/dist/js/'
             },
@@ -104,50 +131,87 @@ module.exports = function (grunt) {
 
         },
 
-
-        // htmlmin: {                                     // Task
-        //   dist: {                                      // Target
-        //     options: {                                 // Target options
-        //       removeComments: true,
-        //       collapseWhitespace: true
-        //     },
-        //     files: {                                   // Dictionary of files
-        //       'dist/index.html': 'src/index.html',     // 'destination': 'source'
-        //       'dist/contact.html': 'src/contact.html'
-        //     }
-        //   },
-        //   dev: {                                       // Another target
-        //     files: {
-        //       'dist/index.html': 'src/index.html',
-        //       'dist/contact.html': 'src/contact.html'
-        //     }
-        //   }
-        // },
-
-
-        watch: {
-            minified: {
-                files: 'src/js/*.js',
-                tasks: ['minified']
-            },
-
-            concat: {
-                files: 'src/js/*.js',
-                tasks: ['concat']
+        cssmin: {
+            minify: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/css/',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'dist/css/',
+                    ext: '.min.css'
+                }, {
+                    expand: true,
+                    cwd: 'views/src/css/',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'views/dist/css/',
+                    ext: '.min.css'
+                }]
             }
         },
+
+        replace: {
+          //TODO: Criar path para arquivos corretamente
+            includeCss: {
+                options: {
+                    patterns: [{
+                        match: 'include_css_style_tag',
+                        replacement: '<%= grunt.file.read("dist/css/style.min.css") %>'
+                    }]
+                },
+                files: [{
+                    expand: true,
+                    flatten: true,
+                    src: ['index.html']
+                    // dest: 'dist/'
+                }]
+            }
+        },
+
+
+        htmlmin: { // Task
+            dist: { // Target
+                options: { // Target options
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: { // Dictionary of files
+                  //TODO: Criar outro arquivo em dist
+                    // 'dist/index.html': 'index.html' // 'destination': 'source'
+                    'index.html': 'index.html' // 'destination': 'source'
+                }
+            },
+        }
+
+
+        // watch: {
+        //     minified: {
+        //         files: 'src/js/*.js',
+        //         tasks: ['minified']
+        //     },
+        //
+        //     concat: {
+        //         files: 'src/js/*.js',
+        //         tasks: ['concat']
+        //     }
+        // },
 
 
     });
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-mkdir');
     grunt.loadNpmTasks('grunt-responsive-images');
-    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-minified');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
-    grunt.registerTask('default', ['clean', 'mkdir', 'responsive_images', 'concat', 'minified', 'watch']);
-    // grunt.registerTask('default', ['clean', 'mkdir', 'concat', 'minified', 'watch']);
+    // grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // grunt.registerTask('default', ['clean', 'mkdir', 'responsive_images', 'concat', 'minified', 'watch']);
+    // grunt.registerTask('default', ['clean', 'mkdir', 'responsive_images', 'imagemin', 'concat', 'minified', 'cssmin', 'replace']);
+    grunt.registerTask('default', ['clean', 'mkdir', 'responsive_images', 'imagemin', 'minified', 'cssmin', 'replace', 'htmlmin']);
+
 
 
 };
--- src/js/teste.js
+++
@@ -1,7 +0,0 @@
-function teste(){
-  console.log("bananinha")
-};
-
-function teste2(){
-  console.log("aí caralho")
-};
